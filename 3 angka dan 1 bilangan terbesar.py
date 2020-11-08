dc5f681177714c463edde13b429ea2b41759d6a3
AkbarXelion <bangkitakbar37@gmail.com>
2020-11-08 14:43:35 +0700

    Update Folder

---
+++ 3 angka dan 1 bilangan terbesar.py
@@ -0,0 +1,10 @@
+a=int(input("Masukkan Angka A : "))
+b=int(input("Masukkan Angka B : "))
+c=int(input("Masukkan Angka C : "))
+
+if a > b and a > c:
+    print(a,"Bilangan terbesar dari 3 bilangan yang diinputkan")
+elif b > a and b > c:
+     print(b,"Bilangan terbesar dari 3 bilangan yang diinputkan")
+else:
+    print(c,"Bilangan terbesar dari 3 bilangan yang diinputkan")
